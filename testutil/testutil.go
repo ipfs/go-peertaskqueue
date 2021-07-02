6f674be0bbba1690164a2d8d2e1ed33785983c15
Marten Seemann <martenseemann@gmail.com>
2021-07-02 12:07:57 -0700

    fix go vet

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -1,6 +1,9 @@
 package testutil
 
-import peer "github.com/libp2p/go-libp2p-core/peer"
+import (
+	"fmt"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
 
 var peerSeq int
 
@@ -9,7 +12,7 @@ func GeneratePeers(n int) []peer.ID {
 	peerIds := make([]peer.ID, 0, n)
 	for i := 0; i < n; i++ {
 		peerSeq++
-		p := peer.ID(peerSeq)
+		p := peer.ID(fmt.Sprintf("%d", peerSeq))
 		peerIds = append(peerIds, p)
 	}
 	return peerIds
